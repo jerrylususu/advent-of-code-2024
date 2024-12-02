bbdab01bc1f833ba95592f3b55c1a97dab811a53
Neko Null <jerrylu.ms@outlook.com>
2024-12-02 23:08:14 +0800

    day2 rs

---
+++ src/bin/day2.rs
@@ -0,0 +1,113 @@
+use std::error::Error;
+use std::io::BufRead;
+use std::{fs::File, io};
+
+fn read_file(path: &str) -> Result<Vec<Vec<i64>>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut result: Vec<Vec<i64>> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let arr = line.split_whitespace()
+            .map(|s| s.parse::<i64>().map_err(|e| e.into()))
+            .collect::<Result<Vec<i64>, Box<dyn Error>>>()?;
+        result.push(arr);
+    }
+
+    Ok(result)
+}
+
+fn check_arr(arr: &Vec<i64>) -> Result<bool, Box<dyn Error>> {
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    enum Direction {
+        Up,
+        Down,
+    }
+
+    let mut last_direction: Option<Direction> = None;
+    
+    for i in 0..arr.len() - 1 {
+        let a1 = arr[i];
+        let a2 = arr[i + 1];
+    
+        let diff = a1 - a2;
+        let diff_abs = diff.abs();
+        if diff_abs < 1 || diff_abs > 3 {
+            return Ok(false);
+        }
+
+        let direction = if a1 > a2 { Direction::Up } else { Direction::Down };
+
+        match last_direction {
+            None => last_direction = Some(direction),
+            Some(last_direction) => {
+                if last_direction != direction {
+                    return Ok(false);
+                }
+            }
+        };
+    }
+
+    Ok(true)
+}
+
+fn solve_p1(input_path: &str) -> Result<usize, Box<dyn Error>> {
+    let arrs = read_file(input_path)?;
+
+    // arrs.iter().filter(|arr| check_arr(arr)).count()
+    arrs.iter().try_fold(0, |count, arr| {
+        Ok(if check_arr(arr)? { count + 1 } else { count })
+    })
+}
+
+fn check_arr_with_skip_idx(arr: &Vec<i64>) -> Result<bool, Box<dyn Error>> {
+    if check_arr(arr)? {
+        return Ok(true);
+    }
+
+    for i in 0..arr.len() {
+        let mut arr_copy = arr.clone();
+        arr_copy.remove(i);
+        if check_arr(&arr_copy)? {
+            return Ok(true);
+        }
+    }
+
+    Ok(false)
+}
+
+fn solve_p2(input_path: &str) -> Result<usize, Box<dyn Error>> {
+    let arrs = read_file(input_path)?;
+    arrs.iter().try_fold(0, |count, arr| {
+        Ok(if check_arr_with_skip_idx(arr)? { count + 1 } else { count })
+    })
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let result_p1 = solve_p1("input.txt")?;
+    println!("Part 1: {}", result_p1);
+
+    let result_p2 = solve_p2("input.txt")?;
+    println!("Part 2: {}", result_p2);
+    
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_p1() {
+        let result = solve_p1("data/day2.txt").unwrap();
+        assert_eq!(result, 591);
+    }
+
+    #[test]
+    fn test_solve_p2() {
+        let result = solve_p2("data/day2.txt").unwrap();
+        assert_eq!(result, 621);
+    }
+}
