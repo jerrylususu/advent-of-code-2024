87654a6a353bf84674a7fc847d3e0780edaf1a2b
Neko Null <jerrylu.ms@outlook.com>
2024-12-02 23:23:43 +0800

    day2 rs rewrite

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -10,7 +10,8 @@ fn read_file(path: &str) -> Result<Vec<Vec<i64>>, Box<dyn Error>> {
 
     for line in reader.lines() {
         let line = line?;
-        let arr = line.split_whitespace()
+        let arr = line
+            .split_whitespace()
             .map(|s| s.parse::<i64>().map_err(|e| e.into()))
             .collect::<Result<Vec<i64>, Box<dyn Error>>>()?;
         result.push(arr);
@@ -19,7 +20,7 @@ fn read_file(path: &str) -> Result<Vec<Vec<i64>>, Box<dyn Error>> {
     Ok(result)
 }
 
-fn check_arr(arr: &Vec<i64>) -> Result<bool, Box<dyn Error>> {
+fn check_arr(arr: &Vec<i64>) -> bool {
     #[derive(Clone, Copy, PartialEq, Eq)]
     enum Direction {
         Up,
@@ -27,69 +28,64 @@ fn check_arr(arr: &Vec<i64>) -> Result<bool, Box<dyn Error>> {
     }
 
     let mut last_direction: Option<Direction> = None;
-    
+
     for i in 0..arr.len() - 1 {
         let a1 = arr[i];
         let a2 = arr[i + 1];
-    
+
         let diff = a1 - a2;
         let diff_abs = diff.abs();
         if diff_abs < 1 || diff_abs > 3 {
-            return Ok(false);
+            return false;
         }
 
-        let direction = if a1 > a2 { Direction::Up } else { Direction::Down };
+        let direction = if a1 > a2 {
+            Direction::Up
+        } else {
+            Direction::Down
+        };
 
         match last_direction {
             None => last_direction = Some(direction),
             Some(last_direction) => {
                 if last_direction != direction {
-                    return Ok(false);
+                    return false;
                 }
             }
         };
     }
 
-    Ok(true)
+    true
 }
 
 fn solve_p1(input_path: &str) -> Result<usize, Box<dyn Error>> {
     let arrs = read_file(input_path)?;
 
-    arrs.iter().map(|arr| 
-        match check_arr(arr) {
-            Ok(true) => Ok(1),
-            Ok(false) => Ok(0),
-            Err(e) => return Err(e),
-        }
-    ).sum()
+    Ok(arrs.iter().filter(|arr| check_arr(arr)).count())
 }
 
-fn check_arr_with_skip_idx(arr: &Vec<i64>) -> Result<bool, Box<dyn Error>> {
-    if check_arr(arr)? {
-        return Ok(true);
+fn check_arr_with_skip_idx(arr: &Vec<i64>) -> bool {
+    if check_arr(arr) {
+        return true;
     }
 
     for i in 0..arr.len() {
         let mut arr_copy = arr.clone();
         arr_copy.remove(i);
-        if check_arr(&arr_copy)? {
-            return Ok(true);
+        if check_arr(&arr_copy) {
+            return true;
         }
     }
 
-    Ok(false)
+    false
 }
 
 fn solve_p2(input_path: &str) -> Result<usize, Box<dyn Error>> {
     let arrs = read_file(input_path)?;
-    arrs.iter().map(|arr| 
-        match check_arr_with_skip_idx(arr) {
-            Ok(true) => Ok(1),
-            Ok(false) => Ok(0),
-            Err(e) => return Err(e),
-        }
-    ).sum()
+    Ok(arrs
+        .iter()
+        .filter(|arr| check_arr_with_skip_idx(arr))
+        .count())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -98,7 +94,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let result_p2 = solve_p2("input.txt")?;
     println!("Part 2: {}", result_p2);
-    
+
     Ok(())
 }
 
